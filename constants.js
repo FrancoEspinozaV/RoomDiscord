637d0ff616f24502955eb53dd81b7592fd974e3d
FrancoEspinozaV <franco.espinoza.val@gmail.com>
2024-06-09 00:59:20 -0400

    file to centralize variables

---
+++ constants.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const Token = process.env.TOKEN
+export const ClientID = process.env.CLIENT_ID
+export const ServerID = process.env.SERVER_ID
+export const creatorChannelID = process.env.CREATOR_CHANNEL_ID
+export const categoryID = process.env.CATEGORY_ID
